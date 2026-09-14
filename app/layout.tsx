32be6b27e0b8deda38b7bbd406b45603fd3a7cbd
Ritik Agarwal <ritikagarwal2468@gmail.com>
2026-09-14 21:51:03 +0530

    chore: replace favicon with new RA monogram

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       suppressHydrationWarning
     >
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </head>
       <body className="bg-bg text-text-primary antialiased">
         <SkipNav />
